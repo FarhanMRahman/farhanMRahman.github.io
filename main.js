d0fd575085ad4bed8d906d9954bee23b17e819e1
farhanMRahman <farhan4839@gmail.com>
2016-12-10 03:32:56 -0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function sendMail()
-{
-    var yourMessage = document.getElementById("message").value;
-    var subject = document.getElementById("subject").value;
-    document.location.href = "mailto:farhan4839@gmail.com?subject="
-        + encodeURIComponent(subject)
-        + "&body=" + encodeURIComponent(yourMessage);
-}
